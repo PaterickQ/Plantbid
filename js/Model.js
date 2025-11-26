9e7010dff33ecd4e92d257f2c013eef118ad75d2
Tomas Jerabek <tomas.jerabek@pedf.cuni.cz>
2025-11-26 10:29:55 +0100

    Metoda View pro vykreslení kamenů, naplneni kamenu + základní CSS

--- js/Model.js
+++ js/Model.js
@@ -1,3 +1,22 @@
 class Model {
     
+    #size;
+    /**
+     * Pole kamenů
+     * 0 znamená prázdné pole
+     * [1,2,3,0]
+     */
+    #stones = [];
+    
+    constructor(size) {
+        this.#size = size;        
+        for(let i = 1; i<size*size; i++) {
+            this.#stones.push(""+i);
+        }
+        this.#stones.push(""+0);        
+    }
+
+    getStones(){
+        return this.#stones;
+    }
 }
